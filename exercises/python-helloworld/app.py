85c1557fa9ba7685551783b610ebeff9948a596f
belachkar <belachkar_ali@hotmail.com>
2021-06-12 06:36:28 +0100

    Add logging & .gitignore file

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,12 +1,34 @@
-from flask import Flask
+from flask import Flask, jsonify
+import logging
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
+    app.logger.info("Main request successfull")
+
     return "Hello World!"
 
 
+@app.route("/status")
+def status():
+    app.logger.info("Status request successfull")
+
+    return jsonify({"result": "OK - healthy"})
+
+
+@app.route("/metrics")
+def metrics():
+    app.logger.info("Metrics request successfull")
+
+    return jsonify(
+        {"status": "success", "code": 0, "data": {"UserCount": 140, "UserCountActive": 23}}
+    )
+
+
 if __name__ == "__main__":
+    # stream logs to a file
+    logging.basicConfig(filename="app.log", level=logging.DEBUG)
+
     app.run(host="0.0.0.0")
